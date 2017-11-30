8a43a26e0f656699ef34d2bdbd893bef74250bb5
coversc <coversc@rose-hulman.edu>
2017-11-29 19:38:04 -0500

    Questions about the code were answered and new turtles added

--- src/m6_using_objects.py
+++ src/m6_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Caitlin Coverstone.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,7 +14,8 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
-
+    two_circles()
+    circle_and_rectangle()
 
 def two_circles():
     """
@@ -26,13 +27,31 @@ def two_circles():
            -- One is filled with some color and one is not filled.
     -- Waits for the user to press the mouse, then closes the window.
     """
+
+    window = rg.RoseWindow(700,400)
+
+    center_point = rg.Point(300, 100)
+    radius = 50
+    circle1 = rg.Circle(center_point, radius)
+    circle1.fill_color = 'green'
+    circle1.attach_to(window)
+
+    center_point2 = rg.Point(100, 300)
+    radius2 = 20
+    circle2 = rg.Circle(center_point2, radius2)
+    circle2.attach_to(window)
+
+    window.render()
+    window.close_on_mouse_click()
+
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its doc-string above.
+    # DONE: 2. Implement this function, per its doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.txt  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # ------------------------------------------------------------------
+
 
 
 def circle_and_rectangle():
@@ -66,6 +85,38 @@ def circle_and_rectangle():
            75.0
            150.0
     """
+
+    #window = rg.RoseWindow(700, 400)
+
+    center_point = rg.Point(300, 100)
+    radius = 50
+    circle = rg.Circle(center_point, radius)
+    circle.fill_color = 'blue'
+
+    circle.attach_to(window)
+    window.render()
+
+    print(circle.center)
+    print(circle.outline_thickness)
+    print(circle.fill_color)
+    print(circle.center.x)
+    print(circle.center.y)
+
+
+    point1 = rg.Point(100, 150)
+    point2 = rg.Point(200, 50)
+    rectangle = rg.Rectangle(point1, point2)
+    rectangle.attach_to(window)
+
+    print(rectangle.get_center())
+    print(rectangle.outline_thickness)
+    print(rectangle.fill_color)
+    print(rectangle.get_center().x)
+    print(rectangle.get_center().y)
+
+    window.render()
+    window.close_on_mouse_click()
+
     # ------------------------------------------------------------------
     # TODO: 3. Implement this function, per its doc-string above.
     #   -- ANY objects that meet the criteria are fine.
